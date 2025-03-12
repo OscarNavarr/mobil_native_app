bdfe7f9473605b5252765573f7977a35c11548c5
Oscar Alejandro <oscarnavarroaviles@gmail.com>
2025-03-12 11:54:51 +0100

    Adding tabs menu

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,11 @@
 import { Text, View } from "react-native";
-import "./global.css"
+import "../global.css"
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center h-full w-full"> 
-      <Text>Welcome!</Text>
-      
+      <Text className="text-5xl text-dark-200">Welcome!</Text>
     </View>
   );
 }
