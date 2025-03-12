cada5533f47ea6bc395f07f612dcd79b4396394d
Oscar Alejandro <oscarnavarroaviles@gmail.com>
2025-03-12 09:49:13 +0100

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import "./global.css"
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center h-full w-full"> 
+      <Text>Welcome!</Text>
+    </View>
+  );
+}
