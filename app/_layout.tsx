e613ce585df27537f077a3f02a31eb517793b397
Oscar Alejandro <oscarnavarroaviles@gmail.com>
2025-03-14 11:52:41 +0100

    Eliminar imágenes no utilizadas y agregar nuevos íconos y tipos de imágenes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,14 +2,16 @@ import { Stack } from "expo-router";
 
 
 export default function RootLayout() {
-  return <Stack>
-    <Stack.Screen
-      name="(tabs)"
-      options={{headerShown: false}}
-    />
-    <Stack.Screen
-      name="movie/[id]"
-      options={{headerShown: false}}  
-    />
-  </Stack>;
+  return (
+    <Stack>
+      <Stack.Screen
+        name="(tabs)"
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="movie/[id]"
+        options={{headerShown: false}}  
+      />
+    </Stack>
+  )
 }
