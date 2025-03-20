089aafdb8a19827c5a613aeb6b69dfe26e25df1a
Oscar Alejandro <oscarnavarroaviles@gmail.com>
2025-03-20 15:34:03 +0100

    Agregar mejoras en la visualización del perfil y detalles de películas, incluyendo nuevos componentes y manejo de navegación

--- app/movies/[id].tsx
+++ app/movies/[id].tsx
@@ -1,12 +1,30 @@
-import { Image, ScrollView, StyleSheet, Text, View } from 'react-native'
+import { Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
-import { useLocalSearchParams } from 'expo-router'
+import { useLocalSearchParams, useRouter } from 'expo-router'
 import useFetch from '@/services/useFetch';
 import { fetchMovieDetails } from '@/services/api';
 import { icons } from '@/constants/icons';
 
+
+interface MovieInfoProps{
+  label: string;
+  value?: string | number | null;
+}
+
+const MovieInfo = ({label, value}: MovieInfoProps) => (
+  <View className='flex-col items-start  justify-center mt-5'>
+    <Text className='text-light-200 font-normal text-sm'>
+      {label}
+    </Text>
+    <Text className='text-light-100 font-bold text-sm mt-2'>
+      {value || 'N/A'}
+    </Text>
+  </View>
+)
+
 const MovieDetails = () => {
 
+  const router = useRouter();
   const {id} = useLocalSearchParams();
 
   const {data: movie, loading} = useFetch(() => fetchMovieDetails(id as string))
@@ -36,9 +54,46 @@ const MovieDetails = () => {
             </Text>
           </View>
 
-          
+          <MovieInfo label="Overview" value={movie?.overview} />
+          <MovieInfo
+            label="Genres"
+            value={movie?.genres?.map((g) => g.name).join(" • ") || "N/A"}
+          />
+
+          <View className="flex flex-row justify-between w-1/2">
+            <MovieInfo
+              label="Budget"
+              value={`$${(movie?.budget ?? 0) / 1_000_000} million`}
+            />
+            <MovieInfo
+              label="Revenue"
+              value={`$${Math.round(
+                (movie?.revenue ?? 0) / 1_000_000
+              )} million`}
+            />
+          </View>
+
+          <MovieInfo
+            label="Production Companies"
+            value={
+              movie?.production_companies?.map((c) => c.name).join(" • ") ||
+              "N/A"
+            }
+          />
         </View>
       </ScrollView>
+
+      <TouchableOpacity
+        className="absolute bottom-5 left-0 right-0 mx-5 bg-accent rounded-lg py-3.5 flex flex-row items-center justify-center z-50"
+        onPress={router.back}
+      >
+        <Image
+          source={icons.arrow}
+          className="size-5 mr-1 mt-0.5 rotate-180"
+          tintColor="#fff"
+        />
+        <Text className="text-white font-semibold text-base">Go Back</Text>
+      </TouchableOpacity>
     </View>
   )
 }
